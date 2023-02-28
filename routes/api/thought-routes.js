8c90742e2304fb78d234833e43ef35a41bb08652
Matt Lahey <mdlahey@outlook.com>
2023-02-28 18:56:02 -0500

    fixed error in file structure

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,38 @@
+const router = require("express").Router();
+
+const {
+  getAllThoughts,
+  getThoughtById,
+  addThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  removeReaction
+} = require("../../controllers/thought-controller");
+
+router
+.route("/")
+.get(getAllThoughts);
+
+router
+.route("/:userId")
+.post(addThought);
+
+router
+.route("/:userId/:thoughtId")
+.delete(deleteThought);
+
+router
+.route("/:id")
+.get(getThoughtById)
+.put(updateThought);
+
+router
+.route("/:thoughtId/reactions")
+.put(addReaction);
+
+router
+.route("/:thoughtId/:reactionId/reactions")
+.delete(removeReaction);
+
+module.exports = router;
